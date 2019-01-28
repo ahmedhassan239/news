afc2a23dfda33013153a3c24aaffc5faa34b1b88
ahmedhassan239 <ahmedhassanelsayed239@gmail.com>
2019-01-28 17:29:23 +0200

    a7a

--- database/migrations/2019_01_27_202335_create_articels_table.php
+++ database/migrations/2019_01_27_202335_create_articels_table.php
@@ -15,6 +15,13 @@ class CreateArticelsTable extends Migration
     {
         Schema::create('articels', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->tinyInteger('published')->comment('0:no,1:yes');
             $table->timestamps();
         });
     }
